ef95aae9e29ec8bf71783294645b9afecc423f7a
KSV <faltuvisitor@yahoo.co.in>
2015-07-09 11:51:19 +0530

    update the decryption method for AnimeDigital

--- AnimeDigital.php
+++ AnimeDigital.php
@@ -6,44 +6,33 @@
   $file = file_get_contents($argv[1]);
   $file = base64_decode($file);
 
-  // Get initial key from aes constants
-  $constants = "\x65\x61\x62\x30\x32\x33\x65\x37\x66\x64\x63\x36\x65\x38\x63\x36\x39\x30\x38\x65\x31\x31\x36\x62\x63\x35\x35\x30\x61\x61\x37\x30\x39\x39\x64\x34\x36\x66\x38\x36\x36\x34\x33\x36\x63\x35\x61\x37\x36\x33\x62\x30\x66\x62\x66\x62\x37\x33\x61\x32\x33\x65\x63\x65\x62\x38\x64\x33\x31\x33\x66\x37\x37\x32\x32\x61\x66\x34\x37\x64\x34\x35\x33\x30\x62\x64\x62\x64\x39\x36\x64\x39\x37\x30\x64\x38\x38\x32\x31\x64\x30\x37\x30\x37\x64\x32\x38\x34\x62\x35\x38\x35\x66\x66\x35";
-  $start     = 31;
+  // Get decryption key from aes constants
+  $constants = "\x66\x38\x64\x33\x66\x66\x32\x35\x37\x63\x32\x61\x32\x61\x30\x33\x66\x34\x62\x61\x33\x61\x37\x34\x34\x36\x33\x30\x30\x31\x65\x66\x64\x32\x38\x34\x38\x66\x64\x35\x32\x35\x36\x30\x36\x35\x32\x62\x65\x33\x62\x37\x33\x38\x31\x63\x35\x33\x38\x66\x35\x34\x30\x39\x65\x35\x39\x32\x31\x30\x66\x34\x63\x32\x30\x62\x63\x63\x66\x65\x30\x64\x30\x33\x30\x66\x33\x64\x34\x39\x62\x33\x65\x31\x39\x33\x38\x36\x62\x36\x62\x66\x66\x33\x30\x62\x33\x36\x36\x30\x37\x30";
+  $start     = 56;
   $key       = substr($constants, $start, 32);
-
-  // Generate key
-  $td  = mcrypt_module_open('rijndael-128', '', 'ecb', '');
-  $iv  = str_repeat("\x00", 16);
-  $key = $key . str_repeat("\x00", 32 - strlen($key));
-  mcrypt_generic_init($td, $key, $iv);
-  $key = mcrypt_generic($td, $key);
-  $key = str_repeat(substr($key, 0, 16), 2);
-  mcrypt_generic_deinit($td);
-
-  /* PHP CTR mode wasn't working as intended so have to decrypt manually */
-
-  // Generate nonce for decryption
-  $encrypted = substr($file, 8);
-  $encLen    = strlen($encrypted);
-  $blocks    = ceil($encLen / 16);
-  $nonce     = substr($file, 0, 8) . pack('N', 0);
+  $salt      = substr($file, 8, 8);
+  $key       = $key . $salt;
+  $hash1     = md5($key, true);
+  $hash2     = md5($hash1 . $key, true);
+  $iv        = md5($hash2 . $key, true);
+  $key       = $hash1 . $hash2;
 
   // Decrypt subtitles
-  $decrypted = " ";
+  $td = mcrypt_module_open('rijndael-128', '', 'cbc', '');
   mcrypt_generic_init($td, $key, $iv);
-  for ($i = 0; $i < $blocks; $i++)
-    {
-      $enc = $nonce . pack('N', $i);
-      $dec = mcrypt_generic($td, $enc);
-      for ($j = 0; $j < 16; $j++)
-        {
-          if (!isset($encrypted[($i * 16) + $j]))
-              break;
-          $decrypted[($i * 16) + $j] = $dec[$j] ^ $encrypted[($i * 16) + $j];
-        }
-    }
+  $decrypted = mdecrypt_generic($td, $file);
+  $decrypted = substr($decrypted, 32);
   mcrypt_generic_deinit($td);
   mcrypt_module_close($td);
+
+  // Detect and remove PKCS#7 padding
+  $padded = true;
+  $len    = strlen($decrypted);
+  $pad    = ord($decrypted[$len - 1]);
+  for ($i = 1; $i <= $pad; $i++)
+      $padded &= ($pad == ord(substr($decrypted, -$i, 1))) ? true : false;
+  if ($padded)
+      $decrypted = substr($decrypted, 0, $len - $pad);
 
   // Save decrypted subtitles
   $file = pathinfo($argv[1], PATHINFO_FILENAME) . ".ass";
